72077a3d4a8ae3e4a648fbcfb2b4d292af2b0725
Govind N Acharya <govindnacharya@gmail.com>
2025-12-31 15:17:58 +0530

    [Govind N Acharya] Add. Added Constructors, Instance Varaibles and Access Modifiers Practice Questions

---
+++ oops-practice/gcr-codebase/constructors-and-instance-variables/CopyConstructorSimulation.java
@@ -0,0 +1,32 @@
+package oops_practice.gcr_codebase.constructors_and_instance_variables;
+class Person {
+    private String name;
+    private int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    Person(Person other) {
+        this.name = other.name;
+        this.age = other.age;
+    }
+
+   
+    void display() {
+        System.out.println(name + " - " + age);
+    }
+}
+
+public class CopyConstructorSimulation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person p1 = new Person("Govind", 21);
+		Person p2 = new Person(p1);
+		p1.display();
+		p2.display();
+	}
+
+}
