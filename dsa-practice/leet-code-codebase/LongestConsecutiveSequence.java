331d7fb324b877300ca49553a23e61eae3dd7910
Govind N Acharya <govindnacharya@gmail.com>
2026-01-19 10:46:15 +0530

    [Govind N Acharya] Add. Added 2 Leetcode questions

---
+++ dsa-practice/leet-code-codebase/LongestConsecutiveSequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int n : nums){
+            set.add(n);
+        }
+        int max = 0;
+        for(int n : set){
+            if(!set.contains(n - 1)){
+                int len = 1;
+                while(set.contains(n + len)){
+                    len++;
+                }
+                max = Math.max(max, len);
+            }
+        }
+        return max;
+    }
+}
