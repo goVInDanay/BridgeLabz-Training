6b66836eaabbf7eb8735cb7a9f49c4c6db4e1334
Govind N Acharya <govindnacharya@gmail.com>
2026-01-19 16:09:21 +0530

    [Govind N Acharya] Add . Added Address Book Scenario Based Question till Use Case 3

---
+++ oops-practice/gcr-codebase/address_book/AddressBookProgram.java
@@ -0,0 +1,7 @@
+package oops_practice.gcr_codebase.address_book;
+
+public class AddressBookProgram {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
+	}
+}
