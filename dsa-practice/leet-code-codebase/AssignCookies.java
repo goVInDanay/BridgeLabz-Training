e8dacb4a7535ba607851af44e25e6cf41987da30
Govind N Acharya <govindnacharya@gmail.com>
2026-02-01 13:22:59 +0530

    [Govind N Acharya] Add. Added 2 LeetCode Based questions

---
+++ dsa-practice/leet-code-codebase/AssignCookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i = 0;
+        int j = 0;
+        int n = g.length;
+        int m = s.length;
+        while(i < n && j < m){
+            if(s[j] >= g[i]){
+                j++;
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i;
+    }
+}
