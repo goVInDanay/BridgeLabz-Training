8d01ebc75502eef8536adc4854becbff958578e3
Govind N Acharya <govindnacharya@gmail.com>
2026-02-09 15:11:42 +0530

    [Govind N Acharya] Add. Added DBMS Practice Problems

--- dbms-practice/gcr-codebase/dbms-practice/src/main/java/com/bridgelabz/io/dbms_practice/DatabaseConnection.java
+++ dbms-practice/gcr-codebase/dbms-practice/src/main/java/com/bridgelabz/io/dbms_practice/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
 	// Database credentials
 	private static final String URL = "jdbc:mysql://localhost:3306/student_db";
 	private static final String USER = "root";
-	private static final String PASSWORD = "12345678";
+	private static final String PASSWORD = "password";
 
 	public static Connection getConnection() {
 		Connection connection = null;
