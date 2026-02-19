ab4c0d9c82bd0e8e2e79afbdd133d569f13fa35f
Govind N Acharya <govindnacharya@gmail.com>
2026-02-19 11:13:08 +0530

    [Govind N Acharya] Add. Added 2 LeetCode Based questions

---
+++ dsa-practice/leet-code-codebase/CounrBinaryString.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int ans = 0;
+        int curr = 1;
+        int prev = 0;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i - 1)){
+                curr++;
+            }
+            else{
+                prev = curr;
+                curr = 1;
+            }
+            if(curr <= prev){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
