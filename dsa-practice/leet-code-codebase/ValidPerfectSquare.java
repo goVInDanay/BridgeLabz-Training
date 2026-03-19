efb33c7fd46b864a313eee73d5f6547a4ccec281
Govind N Acharya <govindnacharya@gmail.com>
2026-03-19 10:53:59 +0530

    [Govind N Acharya] Add. Added 2 LeetCode Based questions

---
+++ dsa-practice/leet-code-codebase/ValidPerfectSquare.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        if(num == 1){
+            return true;
+        }
+        long start = 1;
+        long end = num;
+        while(start <= end){
+            long mid = start + (end - start) / 2;
+            if(mid * mid == num){
+                return true;
+            }
+            else if(mid * mid > num){
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return false;
+    }
+}   
